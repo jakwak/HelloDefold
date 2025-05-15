1a5461579fbc49c2636d54f685bae379cb33ab80
Kwak Jaehak <ondalssam@gmail.com>
2025-05-15 22:22:17 +0900

    ...

---
+++ player/other_player.go
@@ -0,0 +1,20 @@
+components {
+  id: "other_player"
+  component: "/player/other_player.script"
+}
+embedded_components {
+  id: "otherplayer"
+  type: "label"
+  data: "size {\n"
+  "  x: 128.0\n"
+  "  y: 32.0\n"
+  "}\n"
+  "color {\n"
+  "  y: 0.0\n"
+  "  z: 0.0\n"
+  "}\n"
+  "text: \"Other Player ^^^^^^^\"\n"
+  "font: \"/builtins/fonts/default.font\"\n"
+  "material: \"/builtins/fonts/label-df.material\"\n"
+  ""
+}
--- player/player.go
+++ player/player.go
@@ -2,3 +2,9 @@ components {
   id: "player"
   component: "/player/player.script"
 }
+embedded_components {
+  id: "other_player_factory"
+  type: "factory"
+  data: "prototype: \"/player/other_player.go\"\n"
+  ""
+}
