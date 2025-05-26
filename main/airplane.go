fd73b6e439bd2e09054bd1ca145704b2231b24a1
Kwak Jaehak <ondalssam@gmail.com>
2025-05-26 21:52:44 +0900

    ...

--- main/airplane.go
+++ main/airplane.go
@@ -1,11 +1,3 @@
-components {
-  id: "airplane_sprite"
-  component: "/main/airplane.sprite"
-  scale {
-    x: 0.5
-    y: 0.5
-  }
-}
 embedded_components {
   id: "airplane2_sprite"
   type: "sprite"
@@ -16,4 +8,19 @@ embedded_components {
   "  texture: \"/main/airplane.atlas\"\n"
   "}\n"
   ""
+}
+embedded_components {
+  id: "airplane_sprite"
+  type: "sprite"
+  data: "default_animation: \"default\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/airplane.atlas\"\n"
+  "}\n"
+  ""
+  scale {
+    x: 0.5
+    y: 0.5
+  }
 }
