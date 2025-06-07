8b0e5701614f1846f71a9d1feea1df94548ae3cf
Kwak Jaehak <ondalssam@gmail.com>
2025-06-07 21:49:57 +0900

    Add missile functionality: introduce missile asset, update bullet spawning logic, and enhance missile handling in the spawner

---
+++ main/missile.go
@@ -0,0 +1,31 @@
+components {
+  id: "bullet"
+  component: "/main/bullet.script"
+}
+components {
+  id: "explode"
+  component: "/main/assets/explode.particlefx"
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"missile\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "size {\n"
+  "  x: 32.0\n"
+  "  y: 32.0\n"
+  "}\n"
+  "size_mode: SIZE_MODE_MANUAL\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/assets/airplane.atlas\"\n"
+  "}\n"
+  ""
+  rotation {
+    z: 0.70710677
+    w: -0.70710677
+  }
+  scale {
+    x: 0.299837
+  }
+}
