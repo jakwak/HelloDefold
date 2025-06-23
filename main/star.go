a4bbf210825c8224bbd2246c321fb3217c3a9e7a
Kwak Jaehak <ondalssam@gmail.com>
2025-06-23 22:47:52 +0900

    Remove unused scripts and assets: delete deprecated wall and player scripts, and clean up related collections and color definitions.

---
+++ main/star.go
@@ -0,0 +1,15 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"star\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/assets/airplane.atlas\"\n"
+  "}\n"
+  ""
+  scale {
+    x: 0.5
+    y: 0.5
+  }
+}
